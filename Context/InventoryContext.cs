e7add0b13135768f8dbf8b0d715542ed7659b927
Alvesx3 <mateusbitc@gmail.com>
2021-09-03 17:28:09 -0300

    DB

--- Context/InventoryContext.cs
+++ Context/InventoryContext.cs
@@ -11,11 +11,11 @@ namespace API.Context
     public class InventoryContext : DbContext
     {
         public DbSet<Inventory> Inventory { get; set; }
-        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        //{
-        //    optionsBuilder.UseMySql("server=localhost;port=3306;database=EFCoreMySQL;user=root;password=hava123;database=ef");
-        //}
-        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql("server=localhost;port=3306;database=EFCoreMySQL;user=root;password=hava123;database=ef",new MariaDbServerVersion(new Version()));
+        }
+
 
     }
 }
---
+++ Migrations/Inventory/20210903202406_tt.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace API.Migrations.Inventory
+{
+    public partial class tt : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Inventory",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Amount = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Inventory", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Inventory");
+        }
+    }
+}
